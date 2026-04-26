a54aa0e36b0875eee48986b99939cf71492779e7
ocelot_FlowX13 <randy_rcr@outlook.com>
2026-04-27 01:54:24 +0200

    Fixed Vulkan instance creation for Mac

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,24 @@ use winit::application::ApplicationHandler;
 use winit::event::WindowEvent;
 use winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop};
 use winit::window::{Window, WindowId};
-use winit::dpi::PhysicalSize;
+use winit::dpi::LogicalSize;
+
+use ash::khr::portability_enumeration::*;
+use ash::vk::InstanceCreateFlags;
 
 // use ash::version::IntanceV1_0;
+
+#[cfg(target_os = "macos")]
+pub fn required_extension_names() -> Vec<*const i8> {
+    vec![
+        KHR_PORTABILITY_ENUMERATION_NAME.name().as_ptr(),
+    ]
+}
+
+#[cfg(not(target_os = "macos"))]
+pub fn required_extension_names() -> Vec<*const i8> {
+    vec![]
+}
 
 const WIDTH: u32 = 800;
 const HEIGHT: u32 = 600;
@@ -24,11 +39,12 @@ impl ApplicationHandler for HelloTriangleApplication {
             .create_window(
                 Window::default_attributes()
                     .with_title("Vulkan tutorial with Ash")
-                    .with_inner_size(PhysicalSize::new(WIDTH, HEIGHT)),
+                    .with_inner_size(LogicalSize::new(WIDTH, HEIGHT)),
             )
             .unwrap();
 
         self.window = Some(window);
+        self.init_vulkan();
     }
 
     fn window_event(&mut self, event_loop: &ActiveEventLoop, _id: WindowId, event: WindowEvent) {
@@ -64,15 +80,20 @@ impl HelloTriangleApplication {
             _marker: std::marker::PhantomData,
         };
 
+        let extension_names = required_extension_names();
+
         let create_info = ash::vk::InstanceCreateInfo {
             s_type: ash::vk::StructureType::INSTANCE_CREATE_INFO,
             p_next: std::ptr::null(),
-            flags: Default::default(),
+            flags: (
+                InstanceCreateFlags::default()
+                | InstanceCreateFlags::ENUMERATE_PORTABILITY_KHR
+            ),
             p_application_info: &app_info,
             enabled_layer_count: 0,
             pp_enabled_layer_names: std::ptr::null(),
-            enabled_extension_count: 0,
-            pp_enabled_extension_names: std::ptr::null(),
+            enabled_extension_count: extension_names.len() as u32,
+            pp_enabled_extension_names: extension_names.as_ptr(),
             _marker: std::marker::PhantomData,
         };
 
@@ -88,6 +109,14 @@ impl HelloTriangleApplication {
     }
 
     fn main_loop(&mut self, _event_loop: &ActiveEventLoop) {
+    }
+}
+
+impl Drop for HelloTriangleApplication {
+    fn drop(&mut self) {
+        unsafe {
+            self.instance.as_ref().unwrap().destroy_instance(None);
+        }
     }
 }
 
