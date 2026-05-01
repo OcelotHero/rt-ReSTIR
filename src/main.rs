d9cff9c8d1d0561e80a70a02560dde55d7d7a388
ocelot_FlowX13 <randy_rcr@outlook.com>
2026-05-01 08:29:22 +0200

    Refactored code to use builder pattern. Added window surface creation

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,10 @@ use winit::application::ApplicationHandler;
 use winit::event::WindowEvent;
 use winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop};
 use winit::window::{Window, WindowId};
+use winit::raw_window_handle::{HasDisplayHandle, HasWindowHandle};
 use winit::dpi::LogicalSize;
 
-use ash::{vk, ext};
+use ash::{vk, ext, khr};
 
 use std::ffi::{CStr, CString};
 use std::os::raw::{c_char, c_void};
@@ -21,13 +22,15 @@ const VALIDATION: ValidationInfo = ValidationInfo {
     required_validation_layers: [ "VK_LAYER_KHRONOS_validation" ],
 };
 
+#[derive(Default)]
 struct QueueFamilyIndices {
     graphics_family: Option<u32>,
+    present_family: Option<u32>,
 }
 
 impl QueueFamilyIndices {
     pub fn is_complete(&self) -> bool {
-        self.graphics_family.is_some()
+        self.graphics_family.is_some() && self.present_family.is_some()
     }
 }
 
@@ -80,8 +83,6 @@ fn vk_to_string(raw_string_array: &[c_char]) -> String {
 fn populate_debug_messenger_create_info(
     create_info: &mut vk::DebugUtilsMessengerCreateInfoEXT
 ) {
-    create_info.s_type =
-        vk::StructureType::DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
     create_info.message_severity =
         vk::DebugUtilsMessageSeverityFlagsEXT::WARNING
         // | vk::DebugUtilsMessageSeverityFlagsEXT::VERBOSE
@@ -97,34 +98,49 @@ fn populate_debug_messenger_create_info(
 struct Engine {
     _entry: ash::Entry,
     instance: ash::Instance,
+    surface_instance: khr::surface::Instance,
+    surface: vk::SurfaceKHR,
     debug_utils_instance: Option<ext::debug_utils::Instance>,
     debug_messenger: Option<vk::DebugUtilsMessengerEXT>,
+
     _physical_device: vk::PhysicalDevice,
     device: ash::Device,
+
     _graphics_queue: vk::Queue,
+    _present_queue: vk::Queue,
 }
 
 impl Engine {
-    pub fn new() -> Result<Self> {
+    pub fn new(window: &Window, required_extensions: &[*const i8]) -> Result<Self> {
         let entry = ash::Entry::linked();
-        let instance = Self::create_instance(&entry)?;
+        let instance = Self::create_instance(&entry, required_extensions)?;
+        let (surface_instance, surface) = Self::create_surface(&entry, &instance, window)?;
         let (debug_utils_instance, debug_messenger) =
             Self::setup_debug_messenger(&entry, &instance)?;
-        let physical_device = Self::pick_physical_device(&instance)?;
-        let (device, graphics_queue) = Self::create_logical_device(&instance, physical_device)?;
+        let physical_device = Self::pick_physical_device(&instance, &surface_instance, &surface)?;
+        let (device, family_indices) =
+            Self::create_logical_device(&instance, physical_device, &surface_instance, &surface)?;
+        let graphics_queue =
+            unsafe { device.get_device_queue(family_indices.graphics_family.context("Graphics")?, 0) };
+        let present_queue =
+            unsafe { device.get_device_queue(family_indices.present_family.context("Present")?, 0) };
 
         Ok(Engine {
             _entry: entry,
             instance,
             debug_utils_instance,
             debug_messenger,
+            surface_instance,
+            surface,
             _physical_device: physical_device,
             device,
             _graphics_queue: graphics_queue,
+            _present_queue: present_queue,
         })
     }
 
-    fn create_instance(entry: &ash::Entry) -> Result<ash::Instance> {
+    fn create_instance(entry: &ash::Entry, required_extensions: &[*const i8])
+        -> Result<ash::Instance> {
         ensure!(
             !VALIDATION.enabled || Self::check_validation_layer_support(entry)?,
             "Validation layers requested, but not available!"
@@ -132,22 +148,18 @@ impl Engine {
 
         let app_name = CString::new("Hello Triangle")?;
         let engine_name = CString::new("No Engine")?;
+        let app_info = vk::ApplicationInfo::default()
+            .application_name(&app_name)
+            .application_version(vk::make_api_version(0, 1, 0, 0))
+            .engine_name(&engine_name)
+            .engine_version(vk::make_api_version(0, 1, 0, 0))
+            .api_version(vk::make_api_version(0, 1, 0, 0));
 
-        let app_info = vk::ApplicationInfo {
-            s_type: vk::StructureType::APPLICATION_INFO,
-            p_next: std::ptr::null(),
-            p_application_name: app_name.as_ptr(),
-            application_version: vk::make_api_version(0, 1, 0, 0),
-            p_engine_name: engine_name.as_ptr(),
-            engine_version: vk::make_api_version(0, 1, 0, 0),
-            api_version: vk::make_api_version(0, 1, 0, 0),
-            _marker: std::marker::PhantomData,
-        };
-
-        let mut required_extensions = vec![
-            #[cfg(target_os = "macos")]
-            vk::KHR_PORTABILITY_ENUMERATION_NAME.as_ptr(),
-         ];
+        let mut required_extensions = required_extensions.to_vec();
+        #[cfg(target_os = "macos")]
+        {
+            required_extensions.push(vk::KHR_PORTABILITY_ENUMERATION_NAME.as_ptr());
+        }
         if VALIDATION.enabled {
             required_extensions.push(vk::EXT_DEBUG_UTILS_NAME.as_ptr());
         }
@@ -162,24 +174,22 @@ impl Engine {
             .map(|layer_name| layer_name.as_ptr())
             .collect();
 
-        let mut create_info = vk::InstanceCreateInfo::default();
-        create_info.s_type = vk::StructureType::INSTANCE_CREATE_INFO;
-        create_info.p_application_info = &app_info;
-        create_info.enabled_extension_count = required_extensions.len() as u32;
-        create_info.pp_enabled_extension_names = required_extensions.as_ptr();
+        let mut create_info = vk::InstanceCreateInfo::default()
+            .application_info(&app_info)
+            .enabled_extension_names(&required_extensions);
+
         #[cfg(target_os = "macos")] {
-            create_info.flags = InstanceCreateFlags::ENUMERATE_PORTABILITY_KHR;
+            create_info = create_info
+                .flags(vk::InstanceCreateFlags::ENUMERATE_PORTABILITY_KHR);
         }
 
+        let mut debug_create_info =
+            vk::DebugUtilsMessengerCreateInfoEXT::default();
+        populate_debug_messenger_create_info(&mut debug_create_info);
         if VALIDATION.enabled {
-            let mut debug_create_info =
-                vk::DebugUtilsMessengerCreateInfoEXT::default();
-            populate_debug_messenger_create_info(&mut debug_create_info);
-
-            create_info.enabled_layer_count = validation_layers.len() as u32;
-            create_info.pp_enabled_layer_names = validation_layers.as_ptr();
-            create_info.p_next = &debug_create_info as
-                *const vk::DebugUtilsMessengerCreateInfoEXT as *const c_void;
+            create_info = create_info
+                .enabled_layer_names(&validation_layers)
+                .push_next(&mut debug_create_info);
         }
 
         let instance = unsafe {
@@ -231,33 +241,50 @@ impl Engine {
         let mut create_info = vk::DebugUtilsMessengerCreateInfoEXT::default();
         populate_debug_messenger_create_info(&mut create_info);
 
-        let debug_utils_loader = ext::debug_utils::Instance::new(entry, instance);
+        let debug_utils_instance = ext::debug_utils::Instance::new(entry, instance);
         let debug_messenger = unsafe {
-            debug_utils_loader
+            debug_utils_instance
                 .create_debug_utils_messenger(&create_info, None)
                 .context("Failed to create Debug Utils Messenger!")?
         };
 
-        Ok((Some(debug_utils_loader), Some(debug_messenger)))
+        Ok((Some(debug_utils_instance), Some(debug_messenger)))
     }
 
-    fn is_device_suitable(instance: &ash::Instance, device: vk::PhysicalDevice) -> bool {
+    fn is_device_suitable(
+        instance: &ash::Instance,
+        device: vk::PhysicalDevice,
+        surface_instance: &khr::surface::Instance,
+        surface: &vk::SurfaceKHR
+    ) -> bool {
         let device_properties = unsafe { instance.get_physical_device_properties(device) };
         let device_name = vk_to_string(&device_properties.device_name);
         println!("\tDevice Name: {}", device_name);
 
-        let indices = Self::find_queue_family(instance, device);
+        let indices = Self::find_queue_family(instance, device, surface_instance, surface);
         indices.is_complete()
     }
 
-    fn find_queue_family(instance: &ash::Instance, device: vk::PhysicalDevice) -> QueueFamilyIndices {
+    fn find_queue_family(
+        instance: &ash::Instance,
+        device: vk::PhysicalDevice,
+        surface_instance: &khr::surface::Instance,
+        surface: &vk::SurfaceKHR
+    ) -> QueueFamilyIndices {
         let queue_families = unsafe { instance.get_physical_device_queue_family_properties(device) };
 
-        let mut queue_family_indices = QueueFamilyIndices { graphics_family: None };
+        let mut queue_family_indices = QueueFamilyIndices::default();
 
         for (index, queue_family) in queue_families.iter().enumerate() {
             if queue_family.queue_count > 0 && queue_family.queue_flags.contains(vk::QueueFlags::GRAPHICS) {
                 queue_family_indices.graphics_family = Some(index as u32);
+            }
+
+            let present_support = unsafe {
+                surface_instance.get_physical_device_surface_support(device, index as u32, *surface)
+            };
+            if queue_family.queue_count > 0 && present_support.unwrap_or(false) {
+                queue_family_indices.present_family = Some(index as u32);
             }
 
             if queue_family_indices.is_complete() {
@@ -268,7 +295,11 @@ impl Engine {
         queue_family_indices
     }
 
-    fn pick_physical_device(instance: &ash::Instance) -> Result<vk::PhysicalDevice> {
+    fn pick_physical_device(
+        instance: &ash::Instance,
+        surface_instance: &khr::surface::Instance,
+        surface: &vk::SurfaceKHR
+    ) -> Result<vk::PhysicalDevice> {
         let physical_devices = unsafe {
             instance
                 .enumerate_physical_devices()
@@ -282,34 +313,40 @@ impl Engine {
 
         let physical_device = physical_devices
             .iter()
-            .find(|&device| { Self::is_device_suitable(&instance, *device) })
+            .find(|&device| { Self::is_device_suitable(&instance, *device, surface_instance, surface) })
             .ok_or_else(|| anyhow!("Failed to find a suitable GPU!"))?;
 
         Ok(*physical_device)
     }
 
-    fn create_logical_device(instance: &ash::Instance, physical_device: vk::PhysicalDevice)
-        -> Result<(ash::Device, vk::Queue)> {
-        let indices = Self::find_queue_family(instance, physical_device);
+    fn create_logical_device(
+        instance: &ash::Instance,
+        physical_device: vk::PhysicalDevice,
+        surface_instance: &khr::surface::Instance,
+        surface: &vk::SurfaceKHR
+    ) -> Result<(ash::Device, QueueFamilyIndices)> {
+        let indices = Self::find_queue_family(instance, physical_device, surface_instance, surface);
+
+        let unique_queue_families = [indices.graphics_family, indices.present_family]
+            .iter()
+            .filter_map(|&family| family)
+            .collect::<std::collections::HashSet<u32>>();
 
         let queue_priority = [1.0_f32];
-        let queue_create_info = vk::DeviceQueueCreateInfo {
-            s_type: vk::StructureType::DEVICE_QUEUE_CREATE_INFO,
-            p_next: std::ptr::null(),
-            flags: vk::DeviceQueueCreateFlags::empty(),
-            queue_family_index: indices.graphics_family.unwrap(),
-            queue_count: 1,
-            p_queue_priorities: queue_priority.as_ptr(),
-            _marker: std::marker::PhantomData,
-        };
+        let queue_create_info = unique_queue_families
+            .iter()
+            .map(|&family| {
+                vk::DeviceQueueCreateInfo::default()
+                    .queue_family_index(family)
+                    .queue_priorities(&queue_priority)
+            })
+            .collect::<Vec<_>>();
 
         let device_features = vk::PhysicalDeviceFeatures::default();
 
-        let mut create_info = vk::DeviceCreateInfo::default();
-        create_info.s_type = vk::StructureType::DEVICE_CREATE_INFO;
-        create_info.queue_create_info_count = 1;
-        create_info.p_queue_create_infos = &queue_create_info;
-        create_info.p_enabled_features = &device_features;
+        let mut create_info = vk::DeviceCreateInfo::default()
+            .queue_create_infos(&queue_create_info)
+            .enabled_features(&device_features);
 
         let validation_layers_raw: Vec<CString> = VALIDATION
             .required_validation_layers
@@ -322,8 +359,7 @@ impl Engine {
             .collect();
 
         if VALIDATION.enabled {
-            create_info.enabled_layer_count = validation_layers.len() as u32;
-            create_info.pp_enabled_layer_names = validation_layers.as_ptr();
+            create_info = create_info.enabled_layer_names(&validation_layers);
         }
 
         let device = unsafe {
@@ -332,9 +368,24 @@ impl Engine {
                 .context("Failed to create Logical Device!")?
         };
 
-        let graphics_queue = unsafe { device.get_device_queue(indices.graphics_family.unwrap(), 0) };
+        Ok((device, indices))
+    }
 
-        Ok((device, graphics_queue))
+    fn create_surface(entry: &ash::Entry, instance: &ash::Instance, window: &Window)
+        -> Result<(khr::surface::Instance, vk::SurfaceKHR)> {
+        let surface = unsafe {
+            ash_window::create_surface(
+                entry,
+                instance,
+                window.display_handle()?.as_raw(),
+                window.window_handle()?.as_raw(),
+                None
+            )
+                .context("Failed to create Surface!")?
+        };
+        let surface_instance = khr::surface::Instance::new(entry, instance);
+
+        Ok((surface_instance, surface))
     }
 }
 
@@ -342,6 +393,7 @@ impl Drop for Engine {
     fn drop(&mut self) {
         unsafe {
             self.device.destroy_device(None);
+            self.surface_instance.destroy_surface(self.surface, None);
             if VALIDATION.enabled {
                 if let (Some(loader), Some(messenger)) = (&self.debug_utils_instance, &self.debug_messenger) {
                     loader.destroy_debug_utils_messenger(*messenger, None);
@@ -367,9 +419,12 @@ impl ApplicationHandler for HelloTriangleApplication {
                     .with_inner_size(LogicalSize::new(WIDTH, HEIGHT)),
             )
             .unwrap();
+        let required_extensions =
+            ash_window::enumerate_required_extensions(window.display_handle().unwrap().as_raw())
+                .expect("Failed to enumerate required extensions for surface creation!");
 
+        self.engine = Some(Engine::new(&window, &required_extensions).expect("Failed to initialize Vulkan Engine!"));
         self.window = Some(window);
-        self.engine = Some(Engine::new().expect("Failed to initialize Vulkan Engine!"));
     }
 
     fn window_event(&mut self, event_loop: &ActiveEventLoop, _id: WindowId, event: WindowEvent) {
